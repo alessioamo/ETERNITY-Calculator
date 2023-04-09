73239932ad3391c27297b5a394cfa72d71cf643a
Sarah Amaniss <100058948+tifawtt@users.noreply.github.com>
2023-04-09 14:32:00 -0400

    Error messages for abx

--- Error/ErrorMessages.py
+++ Error/ErrorMessages.py
@@ -5,6 +5,16 @@ ErrorMessages = {
     "Functions": {
         "Acos": {
             "InvalidInputOf1": "Acos cannot take an input of 1"
+        },
+        "abx_a": {
+            "InvalidInputOf0": "a cannot be equal to 0"
+        },
+        "abx_b": {
+            "InvalidInputOf1": "b cannot be equal to 1"
         }
     }
+
+
+
+
 }
--- Widget/Button/FunctionButton/abx.py
+++ Widget/Button/FunctionButton/abx.py
@@ -15,12 +15,17 @@ class abxButton(CalculatorFunctionButton):
 
         # Verify input
         if (a == 0) or (not isinstance(a, (int, float))):
-            print("wrong!")
+            raise InvalidInputError(
+                ErrorMessages["Functions"]["abx_a"]["InvalidInputOf0"]
+            )
 
         if (b == 1) or (not isinstance(b, (int, float))):
-            print("wrong!")
+            raise InvalidInputError(
+                ErrorMessages["Functions"]["abx_b"]["InvalidInputOf1"]
+            )
 
         if not isinstance(x, (int, float)):
-            print("wrong!")
+            raise InvalidInputError(
+                ErrorMessages["Functions"]["General"])
 
         return (a*(b**x))
