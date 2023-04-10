d246723819d839e7978ef3782c91b925b185b41a
Alessio <alessio.amorelli@hotmail.com>
2023-04-10 17:09:39 -0400

    Merge complete

--- Error/ErrorMessages.py
+++ Error/ErrorMessages.py
@@ -7,6 +7,9 @@ ErrorMessages = {
             "InputOutsideDomain": "Acos can only take input that is in [-1,1]",
             "NoParameterGiven": "Acos must take 1 parameter",
             "MoreThan1ParameterGiven": "Acos can only take 1 parameter",
+        },
+        "Log": {
+            "InvalidInputOfNotPositive": "x and base must be positive numbers"
         }
     }
 }
